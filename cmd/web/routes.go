fcb63ff7540bbc7eb47f40a3ec5f9db702bd1a2c
PurnaChand <purna7788@gmail.com>
2024-04-10 23:52:37 +0530

    Created html templates for different pages

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,8 +15,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-	//mux.Get("/",http.HandlerFunc(handlers.Repo.Home))
-	//mux.Get("/about",http.HandlerFunc(handlers.Repo.About))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
 	return mux
 }
