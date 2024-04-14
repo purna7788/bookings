e30d9661e6f56cdf5ed43315015b3339a2c1ad07
PurnaChand <purna7788@gmail.com>
2024-04-14 23:10:51 +0530

    converted htmls to git templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -36,3 +36,28 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 	render.RenderTemplate(w, "about.page.tmpl", models.TemplateData{StringMap: stringMap})
 }
+
+// Generals renders room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", models.TemplateData{})
+}
+
+// Majors renders room page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", models.TemplateData{})
+}
+
+// Reservation renders the make reservation page and displays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", models.TemplateData{})
+}
+
+// Availability renders the make reservation page and displays form
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", models.TemplateData{})
+}
+
+// Availability renders the make reservation page and displays form
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", models.TemplateData{})
+}
